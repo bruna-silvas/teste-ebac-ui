7b42b60cefef4d699ad1f4aa4b40ff2a69cf6f7f
bruna-silvas <bruna.maciel96@outlook.com>
2024-01-04 17:44:19 -0300

    Usando massa de dados

--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -3,7 +3,7 @@
 describe('Funcionalidade Página de Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        cy.visit('produtos')
     });
 
     it('Deve selecionar um produto da lista', () => {
@@ -15,7 +15,7 @@ describe('Funcionalidade Página de Produtos', () => {
     });
 
 
-    it.only('Deve adicionar um produto no carrinho', () => {
+    it('Deve adicionar um produto no carrinho', () => {
         var quantidade = 2
 
         cy.get('[class="product-block grid"]')
